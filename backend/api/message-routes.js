30089fa4002b6e6bfc4fbb81c8e0daf2c7f79af2
Vlad Tregubov <treguv@uw.edu>
2021-07-24 16:19:47 -0700

    added reset schema:

--- backend/api/message-routes.js
+++ backend/api/message-routes.js
@@ -2,23 +2,26 @@ const router = require('express').Router();
 
 //get all messages 
 router.get("/", (req, res) => {
-    console.log("messages get req recieved")
+    console.log("messages get req recieved");
     res.send("Recieved...");
 });
 
 //Post a message
 router.post("/", (req, res) => {
-    console.log("messages post req recieved")
+    console.log("messages post req recieved");
+    res.send("Recieved...");
 });
 
 //delete a message
 router.delete("/", (req, res) => {
-    console.log("messages delete req recieved")
+    console.log("messages delete req recieved");
+    res.send("Recieved...");
 });
 
 //put a  message
 router.put("/", (req, res) => {
-    console.log("messages  put req recieved")
+    console.log("messages  put req recieved");
+    res.send("Recieved...");
 })
 
 module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS smart_frame_db;
+
+CREATE DATABASE smart_frame_db;
