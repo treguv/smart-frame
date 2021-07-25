a6e2c35647b4f086bfb1b7e9d4f132ecfa653dc4
Vlad Tregubov <treguv@uw.edu>
2021-07-24 17:50:34 -0700

    set up sequelize connection to a the local db

--- backend/sqldb/connection.js
+++ backend/sqldb/connection.js
@@ -8,12 +8,15 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  console.log("trying to connect")
     //otherwise connect to the local db
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
   })
+  console.log("Connected successfully");
+
 
 };
 
--- server.js
+++ server.js
@@ -1,13 +1,24 @@
-const routes = require('./backend')
-const express = require('express')
-const sequelize = require('./backend/sqldb/connection')
-const app = express()
- 
+const routes = require('./backend');
+const express = require('express');
+const sequelize = require('./backend/sqldb/connection');
+const path = require('path');
+const app = express();
+//set up port for when it is in prod
+const PORT = process.env.PORT || 3002;
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
+// //express configs
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
-app.listen(3000);
+
+
+//change force to true to reset db on startup
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+});
 console.log("started");
